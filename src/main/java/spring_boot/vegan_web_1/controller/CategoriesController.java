cbed3c0fb0160a276deaca724327f83591737fa4
Maria Caro <github.maria.caro@gmail.com>
2023-01-20 17:40:57 +0000

    Create a new @Controller to manage the Categories which a Recipe belongs to (Breakfast, Lunch, Dinner, etc.)

--- src/main/java/spring_boot/vegan_web_1/controller/CategoriesController.java
+++ src/main/java/spring_boot/vegan_web_1/controller/CategoriesController.java
@@ -1,4 +1,29 @@
 package spring_boot.vegan_web_1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class CategoriesController {
+
+  // @GetMapping("/index")
+  @RequestMapping(value = "/index", method = RequestMethod.GET)
+  public String showIndex(Model model) {
+    return "categories/listCategories";
+  }
+
+  // @GetMapping("/create")
+  @RequestMapping(value = "/create", method = RequestMethod.GET)
+  public String create() {
+    return "categories/formCategory";
+  }
+
+  // @GetMapping("/save")
+  @RequestMapping(value = "/save", method = RequestMethod.POST)
+  public String save() {
+    return "categories/listCategories";
+  }
 }
